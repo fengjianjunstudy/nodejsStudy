c82d8eed4b86b4e21099909e3a333e83c5c6e8d5
fjj_321 <fjj_321@126.com>
2016-06-14 22:48:19 +0800

    chat

--- chat_room/index.js
+++ chat_room/index.js
@@ -1,10 +1,19 @@
 var net =require("net");
 var count=0;
+var clientList = [];
 var server=net.createServer(function(connection){ //这个回调函数在有新的连接建立时被执行
-	console.log()
 	connection.write("welcome to "+count+"  please write your name and press enter:")
 	count++;
-	console.log("\033[90m  new connection!   \033[39m")
+	clientList.push(connection);
+	console.log(clientList);
+	console.log("\033[90m  new connection!   \033[39m");
+	connection.on("data",function(data){
+		for(var i=0;i<clientList.length;i++){
+			if(connection !== clientList[i]){
+				clientList[i].write(data.toString());
+			}
+		}
+	});
 });
 server.listen(3000,function(){ // listen 将服务器绑定在某个端口上
 	console.log("\033[96m  server listening on   \033[39m")
